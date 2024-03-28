fa1f55c65d527f368b51f158e20489c03f3a69d9
Tom Mens <tom.mens@umons.ac.be>
2024-03-28 14:34:32 +0100

    projects

---
+++ projects/disse/home.php
@@ -0,0 +1,26 @@
+<h1 id="introduction" style="border-top: none; padding-top: 0;">Data-Intensive Software System Evolution</h1>
+  
+<h2>Summary</h2>
+
+<p>
+	Since 2008, we started to study how open source software systems evolve over time. The advantages of studying these systems are manifold: (a) there is a huge amount of historical data available for study, often covering more than 10 years of development effort; (b) the data is freely accessible, favorising reproducibility of results; (c) many of these systems are very popular and very complex, raising both a practical and scientific interest. Our research combines empirical studies with software repository mining, an active field of research in the domain of software engineering.
+</p>
+<p>
+	The proposed research project aims to build further on this line of research, by focusing on repository mining and empirical analysis of the evolution of data-intensive software systems, with an emphasis on the co-evolution between program code and database schemas, as well as on the social aspects of software development: communities, collaboration, communication and interaction between human actors.
+</p>
+<p>
+The main interest in this research project is to <strong>observe</strong> the co-evolution between databases and source code, in order to better understand the underlying trends, practices, laws and challenges. This will require the analysis of large-scale data-intensive systems, by developing innovative software repository mining techniques and conducting careful empirical studies. 	
+</p>
+
+
+<h2>Objectives</h2>
+
+<p>
+The main research goal of this project is to study and understand the database usage practices in data-intensive software systems (DISS), how these practices evolve over time, and how to improve upon these practices. By DISS, we mean software systems that rely to an important extent on database technologies, and that need to manage and interact with large quantities of data. These DISS are becoming increasingly more relevant and prevalent, as can be witnessed by an increasing interest (both from an academic and industrial perspective) in big data technologies (such as NoSQL databases, for example).
+</p>
+<p>
+During this project we will develop a theoretical framework that will be empirically validated and calibrated through statistical analysis of the interaction between programs (source and executable code) and databases, in order to identify particular trends, trend breaks, usage patterns, bad practices and good practices, and in order to identify the most popular technologies and paradigm switches at a given point in time. We will also study the co-evolution between programs and databases, in order to identify evolutionary patterns and migration scenarios to different database technologies, and to provide recommendations when carrying out particular database evolution tasks.
+</p>
+<p>
+An important aspect of our research is that we will not restrict ourselves to the artefacts of a DISS (such as program code, executable code, database schemas and the database contents). In addition, we will also consider the social aspects, by analysing the interaction between the people involved in the development and maintenance of such DISS. As an end result of our project we aim to be able to recommend, based on sound arguments, the most qualified (group of) person(s) for achieving specific database-related DISS maintenance activities.
+</p>
---
+++ projects/disse/team.php
@@ -0,0 +1,26 @@
+<h1>Team</h1>
+
+<h2>Project leaders</h2>
+
+<ul>
+	<li><a href="http://w3.umons.ac.be/staff/Mens.Tom">Tom Mens</a>, <a href="http://informatique.umons.ac.be/genlog/">Software Engineering Lab</a>, <a href="https://portail.umons.ac.be/FR/Pages/default.aspx">Université de Mons</a></li>
+	<li><a href="http://directory.unamur.be/staff/acleve">Anthony Cleve</a>, <a href="http://www.unamur.be/en/precise/">PReCISE research center</a>, <a href="http://www.unamur.be/">Université de Namur</a>
+</ul>
+
+<h2>PhD Students</h2>
+
+<ul>
+	<li><a href="http://directory.unamur.be/staff/lmeurice?_LOCALE_=en">Loup Meurice</a>, <a href="http://www.unamur.be">Université de Namur</a>
+</ul>
+
+<h2>Postdocs</h2>
+
+<ul>
+	<li><a href="http://w3.umons.ac.be/staff/Goeminne.Mathieu/">Mathieu Goeminne</a>, <a href="http://informatique.umons.ac.be/genlog/">Software Engineering Lab</a>, <a href="https://portail.umons.ac.be/FR/Pages/default.aspx">Université de Mons</a></li>
+</ul>
+
+<h2>Collaborator</h2>
+
+<ul>
+	<li><a href="http://decan.lexpage.net/">Alexandre Decan</a>, <a href="http://informatique.umons.ac.be/ssi/">Service des Systèmes d'Information</a>, <a href="https://portail.umons.ac.be/FR/Pages/default.aspx">Université de Mons</a></li>	
+</ul>
