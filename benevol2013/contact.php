4ba06b51e3f273750cf34f5bc47d914e59790336
Tom Mens <tommens@users.noreply.github.com>
2024-03-28 14:06:46 +0100

    Delete benevol2013 directory

--- benevol2013/contact.php
+++
@@ -1,9 +0,0 @@
-<h1>Contact</h1>
-
-<p>For any information, please contact
-	<ul>
-		<li><a href="mailto:tom.mens@umons.ac.be">Tom Mens</a></li>
-		<li><a href="mailto:maelick.claes@umons.ac.be">Maëlick Claes</a></li>
-		<li><a href="mailto:mathieu.goeminne@umons.ac.be">Mathieu Goeminne</a></li>
-	</ul>
-</p>
